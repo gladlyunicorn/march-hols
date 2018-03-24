abf858ce82cdaf8b8c89b7491108e6810e840122
gladlyunicorn <36921343+gladlyunicorn@users.noreply.github.com>
2018-03-24 08:52:53 +0800

    GA2 submission

--- assignments/02-identification/get-details.py
+++ assignments/02-identification/get-details.py
@@ -1,7 +1,9 @@
-name = input("What is your name? ")
+original=raw_input("What is your name")
 
 # If length of name is greater than 20,
 # print something
+if len(original)>20:
+    print"wow that's a long name"
 
 
 
@@ -10,16 +12,22 @@ age = input("What is your age? ")
 # If age is less than 10, print "Smol"
 # ELse if age is between 10 and 20, print "Big boi"
 # Else, print "Big big boi"
+if age<10: 
+    print "Smol"
+elif age>10 and age<20:
+    print "Big big boi"
 
 
 
 coolness = input("Rate your coolness out of 100.0")
 
 # If coolness is more than 100.0, just print some error
+if coolness>100.0:
+    print"error"
 
 
 
 # Now print a string like
 # My name is Arnold Tan, I am 69 and I'm Really Cool
 
-
+print "My name is Arnold Tan, I am 69 and I'm really cool"
